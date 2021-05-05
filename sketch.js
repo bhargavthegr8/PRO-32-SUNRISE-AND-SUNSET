4d4c91c8786e11c982e3707a0682af3c974eca76
bhargavthegr8 <73594912+bhargavthegr8@users.noreply.github.com>
2021-05-05 18:08:47 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -33,41 +33,42 @@ async function getBackgroundImg(){
     var dt = responseJson.datetime;
     var hr = dt.slice(11,13);
 
-    if(hr>04 && hr<06){
+    if(hr>=04 && hr<=06){
         bg = "sunrise1.png"
     }
-    else if(hr>06 && hr<08){
+    else if(hr>=06 && hr<=08){
         bg = "sunrise2.png"
     }
-    else if(hr>08 && hr<10){
+    else if(hr>=08 && hr<=10){
         bg = "sunrise3.png"
     }
-    else if(hr>10 && hr<12){
+    else if(hr>=10 && hr<=12){
         bg = "sunrise4.png"
     }
-    else if(hr>12 && hr<14){
+    else if(hr>=12 && hr<=14){
         bg = "sunrise5.png"
     }
-    else if(hr>14 && hr<16){
+    else if(hr>=14 && hr<=16){
         bg = "sunset6.png"
     }
-    else if(hr>16 && hr<18){
+    else if(hr>=16 && hr<=18){
         bg = "sunset7.png"
     }
-    else if(hr>18 && hr<20){
+    else if(hr>=18 && hr<=20){
         bg = "sunset8.png"
     }
-    else if(hr>20 && hr<22){
+    else if(hr>=20 && hr<=22){
         bg = "sunset9.png"
     }
-    else if(hr>22 && hr<00){
+    else if(hr>=22 && hr<=00){
         bg = "sunset10.png"
     }
-    else if(hr>0 && hr<02){
+    else if(hr>=0 && hr<=02){
         bg = "sunset11.png"
     }
     else{
         bg = "sunset12.png"
     }
+    console.log(bg)
     backgroundImg = loadImage(bg);
 }
